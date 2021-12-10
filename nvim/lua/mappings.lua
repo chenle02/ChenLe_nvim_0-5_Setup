d3fd190a6c33fc68c825d999018e368f2ce9e1c6
chenle02 <chenle02@gmail.com>
2021-12-10 00:10:31 -0500

    Starting working on the configurations.

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,42 @@
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local opt = {}
+
+-- Open neovim config
+map("n", "<leader>ev", ":vsplit ~/.config/nvim/init.lua<CR>", opt)
+map("n", "<leader>sv", ":source ~/.config/nvim/init.lua<CR>", opt)
+
+-- Enforcing Purity for myself to not use arrow keys
+map("n", "<UP>", "<Nop>", opt)
+map("n", "<Down>", "<Nop>", opt)
+map("n", "<Left>", "<Nop>", opt)
+map("n", "<Right>", "<Nop>", opt)
+
+-- Mappings to move lines
+map("n", "<A-j>", ":m .+1<CR>", {noremap=true, silent=true})
+map("n", "<A-k>", ":m .-2<CR>", {noremap=true, silent=true})
+map("i", "<A-j>", ":m .+1<CR>", {noremap=true, silent=true})
+map("i", "<A-k>", ":m .-2<CR>", {noremap=true, silent=true})
+map("v", "<A-j>", ":m .+1<CR>", {noremap=true, silent=true})
+map("v", "<A-k>", ":m .-2<CR>", {noremap=true, silent=true})
+
+-- Use Ctrl+H,J,K,L to navigate panes
+map("n", "<C-h>", "<C-w>h", {noremap=true, silent=true})
+map("n", "<C-j>", "<C-w>j", {noremap=true, silent=true})
+map("n", "<C-k>", "<C-w>k", {noremap=true, silent=true})
+map("n", "<C-l>", "<C-w>l", {noremap=true, silent=true})
+
+-- better indenting
+map('v', '<', '<gv', {noremap = true, silent = true})
+map('v', '>', '>gv', {noremap = true, silent = true})
+
+-- Tabbing through buffers
+map("n", "<C-Right>", ":bn<CR>")  -- Next buffer in list
+map("n", "<C-Left>", ":bp<CR>")   -- Previous buffer in list
+map("n", "<C-S-Left>", ":b#<CR>") -- Previous buffer you were in
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Transition from nvim 0.4 to 0.5.
+1. Following https://vinversion.hashnode.dev/switching-from-neovim-04-to-05 to upgrade to nvim 0.5
